b2934e39c449a0575d6622aea01881814f68a894
Abhinav Sarkar <abhinav@abhinavsarkar.net>
2024-12-05 19:02:02 +0530

    Improves solutions for day 4

--- app/Day4.hs
+++ app/Day4.hs
@@ -1,45 +1,48 @@
 module Main where
 
 import AoC qualified
-import AoC.List (iterateMaybe, ordNub, slidingWindows)
-import Data.Function ((&))
+import Control.Arrow ((>>>))
+import Data.Massiv.Array (Ix2 (..), Sz (..))
 import Data.Massiv.Array qualified as A
-import Math.Geometry.Grid qualified as G
-import Math.Geometry.Grid.OctagonalInternal qualified as G
 
-type Input = A.Array A.U A.Ix2 Char
+type Input = A.Array A.P Ix2 Char
 
 main :: IO ()
-main = AoC.main solvePart1 solvePart2 $ A.fromLists' A.Seq . lines
+main =
+  AoC.main solvePart1 solvePart2 $
+    lines
+      >>> map (\row -> "..." <> row <> "...")
+      >>> ( \rows ->
+              let padding = replicate 3 $ replicate (length $ head rows) '.'
+               in padding <> rows <> padding
+          )
+      >>> A.fromLists' A.Seq
 
 solvePart1 :: Input -> Int
 solvePart1 input =
-  let A.Sz2 rowCount colCount = A.size input
-      grid = G.rectOctGrid rowCount colCount
-   in grid
-        & G.indices
-        & concatMap (\idx -> map (vista grid idx) directions)
-        & concatMap (slidingWindows 4)
-        & ordNub
-        & map (map (input A.!))
-        & filter (== "XMAS")
-        & length
-  where
-    vista grid idx dir =
-      map (uncurry A.Ix2) $ iterateMaybe (flip (G.neighbour grid) dir) idx
-    directions =
-      [G.West, G.Northwest, G.North, G.Northeast, G.East, G.Southeast, G.South, G.Southwest]
+  length
+    [ ()
+      | let Sz2 rowCount colCount = A.size input,
+        row <- [0 .. rowCount - 4],
+        col <- [0 .. colCount - 4],
+        let subGrid = A.compute @A.P $ A.extract' (row :. col) (Sz $ 4 :. 4) input,
+        word <-
+          map
+            (map (subGrid A.!) . flip map [0 .. 3])
+            [(0 :.), (:. 0), \i -> i :. i, \i -> 3 - i :. i],
+        word `elem` ["XMAS", "SAMX"]
+    ]
 
 solvePart2 :: Input -> Int
 solvePart2 input =
-  let A.Sz2 rowCount colCount = A.size input
-   in length $
-        [ subGridStr
-          | row <- [0 .. rowCount - 3],
-            col <- [0 .. colCount - 3],
-            let subGrid = A.compute @A.U $ A.extract' (row A.:. col) (A.Sz (3 A.:. 3)) input,
-            let subGridStr = map ((subGrid A.!) . uncurry A.Ix2) masIdxs,
-            subGridStr `elem` ["MMASS", "MSAMS", "SMASM", "SSAMM"]
-        ]
+  length
+    [ ()
+      | let Sz2 rowCount colCount = A.size input,
+        row <- [0 .. rowCount - 3],
+        col <- [0 .. colCount - 3],
+        let subGrid = A.compute @A.P $ A.extract' (row :. col) (Sz $ 3 :. 3) input,
+        let word = map ((subGrid A.!) . uncurry Ix2) masIdxs,
+        word `elem` ["MMASS", "MSAMS", "SMASM", "SSAMM"]
+    ]
   where
     masIdxs = [(0, 0), (0, 2), (1, 1), (2, 0), (2, 2)]
