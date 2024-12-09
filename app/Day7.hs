12e34bb2f596a7f340153615f40068069c4da62c
Abhinav Sarkar <abhinav@abhinavsarkar.net>
2024-12-09 10:14:30 +0530

    Improves solutions for day 7

--- app/Day7.hs
+++ app/Day7.hs
@@ -1,7 +1,9 @@
 module Main where
 
 import AoC qualified
+import AoC.List (ensure)
 import Data.Char (isDigit)
+import Data.List (isSuffixOf)
 
 main :: IO ()
 main = AoC.main solvePart1 solvePart2 $ map parseLine . lines
@@ -9,16 +11,21 @@ main = AoC.main solvePart1 solvePart2 $ map parseLine . lines
     parseLine s =
       let (res : args) = words s in (read $ takeWhile isDigit res, map read args)
 
-    solvePart1 = solve [(+), (*)]
-    solvePart2 = solve [(+), (*), conc]
+    solvePart1 = solve [mSub, mDiv]
+    solvePart2 = solve [mSub, mDiv, mSplit]
 
     solve ops resArgs =
-      sum [res | (res, args) <- resArgs, res `elem` eval ops res [] args]
+      sum [res | (res, first : rest) <- resArgs, first `elem` eval ops first [res] (reverse rest)]
 
     eval _ _ ress [] = ress
-    eval ops finalRes [] (a : as) = eval ops finalRes [a] as
-    eval ops finalRes ress (a : as) =
-      flip (eval ops finalRes) as $
-        [res' | res <- ress, op <- ops, let res' = res `op` a, res' <= finalRes]
+    eval ops first ress (a : as) =
+      flip (eval ops first) as $
+        [res' | res <- ress, op <- ops, Just res' <- [res `op` a], res' >= first]
 
-    conc a b = read $ show a <> show b
+    mSub a b = ensure (> 0) $ a - b
+    mDiv a b = let (q, r) = a `quotRem` b in if r == 0 then Just q else Nothing
+    mSplit a b =
+      let len = length (show a) - length (show b)
+       in if show b `isSuffixOf` show a && len > 0
+            then Just $ read $ take len $ show a
+            else Nothing
